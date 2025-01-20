fba7c330abb0ef6153ad6dd72c007ef009cd1e94
halfaider <castwhendamagedealt@gmail.com>
2025-01-21 00:56:32 +0900

    v0.3.16

---
+++ __init__.py
@@ -0,0 +1,31 @@
+__version__ = '0.3.15'
+
+import sys
+import subprocess
+
+ARGS = ('-m', 'pip', 'install', '-U')
+
+try:
+    __import__('requests')
+except:
+    subprocess.check_call([sys.executable, *ARGS, 'requests'])
+
+try:
+    __import__('yaml')
+except:
+    subprocess.check_call([sys.executable, *ARGS, 'pyyaml'])
+
+try:
+    __import__('googleapiclient')
+except:
+    subprocess.check_call([sys.executable, *ARGS, 'google-api-python-client'])
+
+try:
+    __import__('google.oauth2')
+except:
+    subprocess.check_call([sys.executable, *ARGS, 'google-auth'])
+
+try:
+    __import__('httplib2')
+except:
+    subprocess.check_call([sys.executable, *ARGS, 'httplib2'])
